2a6238e35937794f8a0730d0beb15acf0382f0f6
Mrinal Dhar <mrinal.dhar@gmail.com>
2016-05-29 20:27:29 +0530

    Added searchbar

---
+++ pedagog/staticfiles/js/endpoints.js
@@ -0,0 +1,12 @@
+// General
+
+var API_login = "/account/login";
+var API_signup = "/account/signup";
+var API_logout = "/account/logout";
+var API_search = "/search";
+
+// Forum
+
+var API_thread = "/forum/thread";
+var API_question = "/forum/question";
+var API_answer = "/forum/answer";
